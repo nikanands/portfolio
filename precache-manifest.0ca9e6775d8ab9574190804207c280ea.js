94fa078a1763032c5e7c094ef3934a0895d65321
Nikhil Anand <nikhil@Nikhils-MacBook-Air.local>
2019-05-21 17:41:57 +0530

    Updates

---
+++ precache-manifest.0ca9e6775d8ab9574190804207c280ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8600769e7b45e124a59b5b2a1171312",
+    "url": "/nikanands/portfolio/static/media/dynamic.d8600769.svg"
+  },
+  {
+    "revision": "a182d40799a036b54bda",
+    "url": "/nikanands/portfolio/static/css/main.41b067b7.chunk.css"
+  },
+  {
+    "revision": "31c00fa8a298df20ad23",
+    "url": "/nikanands/portfolio/static/js/runtime~main.dc869c39.js"
+  },
+  {
+    "revision": "e6fb8790e8118cba7a71",
+    "url": "/nikanands/portfolio/static/js/2.9ec49057.chunk.js"
+  },
+  {
+    "revision": "b92afa7115665e23522238b36d297a02",
+    "url": "/nikanands/portfolio/static/media/WhatsApp Image 2019-05-15 at 3.19.52 PM.b92afa71.jpeg"
+  },
+  {
+    "revision": "a3d5e7c98b5da0a657cf8c3d68cd0d0c",
+    "url": "/nikanands/portfolio/static/media/fast.a3d5e7c9.svg"
+  },
+  {
+    "revision": "4a6f25a896dbf652b778e2eb66df777f",
+    "url": "/nikanands/portfolio/static/media/responsive.4a6f25a8.svg"
+  },
+  {
+    "revision": "3646622251654d7c0cd4f71dff95b38c",
+    "url": "/nikanands/portfolio/static/media/idea.36466222.svg"
+  },
+  {
+    "revision": "a182d40799a036b54bda",
+    "url": "/nikanands/portfolio/static/js/main.c1843fea.chunk.js"
+  },
+  {
+    "revision": "6da7739ee89712b061b995cddd080909",
+    "url": "/nikanands/portfolio/static/media/payment-card.6da7739e.png"
+  },
+  {
+    "revision": "69acf5d405e72e8a2794ba302df24160",
+    "url": "/nikanands/portfolio/static/media/shoppy.69acf5d4.png"
+  },
+  {
+    "revision": "a683780292dc7f4a078d3d761a276734",
+    "url": "/nikanands/portfolio/static/media/keyboard.a6837802.png"
+  },
+  {
+    "revision": "79f7ad94dbd63968c95aa71145019593",
+    "url": "/nikanands/portfolio/static/media/login.79f7ad94.png"
+  },
+  {
+    "revision": "fe5912583a0b44d944a20eb61d839eef",
+    "url": "/nikanands/portfolio/static/media/signup.fe591258.png"
+  },
+  {
+    "revision": "c90a62cdfdc55f0ca1242b4c58f98ae9",
+    "url": "/nikanands/portfolio/static/media/convertico.c90a62cd.png"
+  },
+  {
+    "revision": "4ff62d65e574ed311f73da7fdb5e3878",
+    "url": "/nikanands/portfolio/index.html"
+  }
+];
